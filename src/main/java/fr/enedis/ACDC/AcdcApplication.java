8f33c6395a16e646e406aee2b75966d40ef5e7db
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2024-10-26 11:22:55 +0200

    first commit

---
+++ src/main/java/fr/enedis/ACDC/AcdcApplication.java
@@ -0,0 +1,13 @@
+package fr.enedis.ACDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/enedis/ACDC/AcdcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enedis.ACDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
